9b70a69f99fddfba91b6d87a22c1f4f653ddfb5a
Jessica Nguyen <jessicanguyen320@gmail.com>
2019-01-31 14:27:13 -0800

    created mock card for user profile component

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,9 +1,24 @@
 import React from "react";
 
 const Profile = () => {
+
+    const profileStyle = {
+        width: "18rem"
+    };
+
     return (
         <div>
-            Profile
+            <div class="card" style={profileStyle}>
+                <img class="card-img-top" src="https://photos.gograph.com/thumbs/CSP/CSP657/cartoon-garbage-can-vector-stock_k22277118.jpg" alt="Card image cap" />
+                <div class="card-body">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Name: Oscar</li>
+                        <li class="list-group-item">Date of Birth: 1/1/2010</li>
+                        <li class="list-group-item">Address: 123 Dumpster Blvd.</li>
+                    </ul>
+                </div>
+            </div>
+
         </div>
     )
 }
