af3baea68299e441e1f14cc1048316b1f690434e
Jessica Nguyen <jessicanguyen320@gmail.com>
2019-02-01 12:38:55 -0800

    fixed some css on login/registration card

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -9,18 +9,17 @@ const Login = () => {
             <div className="card">
                 <div className="card-body">
                     <h3>Please Sign In</h3>
+                    <br />
                     <form>
-                        <label className="col-sm-3" for="inputUsername">Login &nbsp;</label>
+                        <label className="col-sm-3" htmlFor="inputUsername">Login &nbsp;</label>
                         <input className="col-sm-4" name="username" type="text" placeholder="Enter Username"></input>
                         <br />
-                        <label className="col-sm-3" for="inputPassword">Password &nbsp;</label>
+                        <label className="col-sm-3" htmlFor="inputPassword">Password &nbsp;</label>
                         <input className="col-sm-4" name="password" type="password" placeholder="Enter Password"></input>
                     </form>
                     <br />
-                    <a href="#">Forgot password?</a>
-                    &nbsp; &nbsp;
-                    <br />
                     <Link to="/register">Not a user? Create new account.</Link>
+                    <br />
                     <br />
                     <button type="button" className="btn btn-info button-green" href="#">Submit</button>
                 </div>
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -9,21 +9,23 @@ const Register = () => {
             <div className="card">
                 <div className="card-body">
                     <h3>Please Register</h3>
+                    <br />
                     <form>
-                        <label className="col-sm-3" for="inputUsername">First Name</label>
+                        <label className="col-sm-3" htmlFor="inputUsername">First Name</label>
                         <input className="col-sm-4" name="firstName" type="text" placeholder="Enter First Name"></input>
                         <br />
-                        <label className="col-sm-3" for="inputUsername">Last Name</label>
+                        <label className="col-sm-3" htmlFor="inputUsername">Last Name</label>
                         <input className="col-sm-4" name="lastName" type="text" placeholder="Enter Last Name"></input>
                         <br />
-                        <label className="col-sm-3" for="inputUsername">Email &nbsp;</label>
+                        <label className="col-sm-3" htmlFor="inputUsername">Email &nbsp;</label>
                         <input className="col-sm-4" name="username" type="text" placeholder="Enter Email"></input>
                         <br />
-                        <label className="col-sm-3" for="inputPassword">Password &nbsp;</label>
+                        <label className="col-sm-3" htmlFor="inputPassword">Password &nbsp;</label>
                         <input className="col-sm-4" name="password" type="password" placeholder="Enter Password"></input>
                     </form>
                     <br />
                     <Link to="/login">Already a user?</Link>
+                    <br />
                     <br />
                     <button type="button" className="btn btn-info button-green" href="#">Submit</button>
                 </div>
