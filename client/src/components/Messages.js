8b799af442c375bba227fe44a0c57be82c8d508a
Ado Moshe <adoran.moshe@gmail.com>
2019-01-31 21:25:40 -0800

    Added messages ui

--- client/src/components/Messages.js
+++ client/src/components/Messages.js
@@ -3,12 +3,36 @@ import 'whatwg-fetch';
 import openSocket from 'socket.io-client';
 const socket = openSocket('http://localhost:3002');
 
-const Messages = () => {
+class Messages extends Component {
+  constructor(props) {
+    super(props);
+    this.sendSocketIO = this.sendSocketIO.bind(this);
+  }
+
+  sendSocketIO() {
+    socket.emit('example_message', 'hello');
+  }
+
+  render() {
     return (
-        <div>
-            Messages
+      <div className="container">
+        <br />
+        <div className="jumbotron">
+          <h1 className="display-4">Send Message</h1>
+          <textarea
+            id="message"
+            className="form-control"
+            placeholder="Your Message Here"
+          />
+          <br />
+          <button onClick={this.sendSocketIO} id="send" className="btn btn-success">
+            Send
+          </button>
         </div>
-    )
+        <div id="messages" />
+      </div>
+    );
+  }
 }
 
 export default Messages;
--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const routes = require('./routes');
 const PORT = process.env.PORT || 3001;
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-const socket = require('socket.io-client')('http://localhost');
 // socket.on('connect', function(){console.log('connected')});
 // socket.on('event', function(data){});
 // socket.on('disconnect', function(){console.log('disconnected')});
@@ -20,12 +19,10 @@ io.on('connection', socket => {
   socket.on('disconnect', () => {
     console.log('User Disconnected');
   });
-  socket.on('example_message', msg => {
-    console.log(`message: ${msg}`);
-  });
+  // socket.on('example_message', msg => {
+  //   console.log(`message: ${msg}`);
+  // });
 });
-
-// io.listen(3001);
 
 // if (process.env.NODE_ENV === 'development') {
 //   app.get('/', function(req, res){
@@ -33,19 +30,15 @@ io.on('connection', socket => {
 // });
 // }
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use((req, res) => {
-//     res.sendFile(path.join(__dirname, '../client/build/index.html'));
-//   });
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
+  });
+}
 
 app.use(routes);
 
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
-
-server.listen(3002, function(){
-  console.log('listening on *:3000');
-});
 
 // Start the API server
 app.listen(PORT, () => {
