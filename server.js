b5190c185d76ce0f01d24425d7178080ad15126c
Ado Moshe <adoran.moshe@gmail.com>
2019-01-31 19:31:32 -0800

    Adding dependencies

--- server.js
+++ server.js
@@ -3,9 +3,23 @@ const express = require('express');
 const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+io.on('connection', socket => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('User Disconnected');
+  });
+  socket.on('example_message', msg => {
+    console.log(`message: ${msg}`);
+  });
+});
+
+io.listen(8000);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('public'));
