52549b1d93d9967b1b258f5ca9243d1ae2969674
Ado Moshe <adoran.moshe@gmail.com>
2019-01-31 20:13:37 -0800

    Requiring socket.io-client

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
-const router = require('express').Router();
 const path = require('path');
 // const mongoose = require("mongoose");
 const routes = require('./routes');
 const PORT = process.env.PORT || 3001;
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+var socket = require('socket.io-client')('http://localhost');
+// socket.on('connect', function(){});
+// socket.on('event', function(data){});
+// socket.on('disconnect', function(){});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -24,19 +27,25 @@ io.on('connection', socket => {
 
 io.listen(8000);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('public'));
-}
+// if (process.env.NODE_ENV === 'development') {
+//   app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, './client/public/index.html'));
+//   });
+// }
 
-if (process.env.NODE_ENV === 'production') {
-  router.use((req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use((req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/build/index.html'));
+//   });
+// }
 
 app.use(routes);
 
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+
+http.listen(3002, () => {
+  console.log('listening on *:3002');
+});
 
 // Start the API server
 app.listen(PORT, () => {
