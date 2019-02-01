f80c8bbac75409cba4e6ddd5cd3dd5d8d757b97d
Ado Moshe <adoran.moshe@gmail.com>
2019-01-31 19:39:42 -0800

    Configuring node environment

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,10 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
-router.use((req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  router.use((req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+}
 
 module.exports = router;
