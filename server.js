1044b7e0f732a50dff552e5d718f7364c933dcd2
Ado Moshe <adoran.moshe@gmail.com>
2019-02-04 13:27:38 -0800

    Routes

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const path = require('path');
-// const mongoose = require("mongoose");
-const routes = require('./routes');
-const PORT = process.env.PORT || 3001;
+// const path = require('path');
+const mongoose = require('mongoose');
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const routes = require('./routes');
+const PORT = process.env.PORT || 3001;
 // socket.on('connect', function(){console.log('connected')});
 // socket.on('event', function(data){});
 // socket.on('disconnect', function(){console.log('disconnected')});
@@ -40,7 +40,9 @@ io.listen(3002);
 
 app.use(routes);
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/reactreadinglist'
+);
 
 // Start the API server
 app.listen(PORT, () => {
