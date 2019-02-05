86839bf3aabcf59e07fea611c8500b9038e8de06
Jessica Nguyen <jessicanguyen320@gmail.com>
2019-02-05 10:13:14 -0800

    testing passport

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -10,18 +10,19 @@ const Login = () => {
                 <div className="card-body">
                     <h3>Please Sign In</h3>
                     <br />
-                    <form>
+                    <form action="/" method="POST">
                         <label className="col-sm-3" htmlFor="inputUsername">Login &nbsp;</label>
                         <input className="col-sm-4" name="username" type="text" placeholder="Enter Username"></input>
                         <br />
                         <label className="col-sm-3" htmlFor="inputPassword">Password &nbsp;</label>
                         <input className="col-sm-4" name="password" type="password" placeholder="Enter Password"></input>
+                        <input type="submit" value="Submit" className="btn btn-info button-green" />
+
                     </form>
                     <br />
                     <Link to="/register">Not a user? Create new account.</Link>
                     <br />
                     <br />
-                    <button type="submit" className="btn btn-info button-green" href="#">Submit</button>
                 </div>
             </div>
         </div >
