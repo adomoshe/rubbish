cd1045a5b7cee6a815a48f8dcbcc59bbe98d839b
sherriefaylin <linsherrie@gmail.com>
2019-02-07 01:52:36 -0800

    wall and posts

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import "../Posts/posts.css"
+import "../Posts/posts.css";
 // import ReactDOM from 'react-dom';
+import oscar from "../Posts/oscar-prof_1.png";
 
 
 
 class Post extends React.Component {
     render() {
     // uh import all posts?
+
+
     var headline = "NEED TRASH HELP"
     // var img = 
     var description = "Looking for someone to help me take out the trash next week!"
@@ -19,7 +22,8 @@ class Post extends React.Component {
     return ( 
         <div className="container">
         <div className="post-box">
-            <h1 className="post-head">{headline}</h1>
+            <h2 className="post-head">{headline}</h2>
+            <img className="post-pic" width="75px" src={oscar} alt="oscar lol"></img>
             <p className="post-descr">{description}</p>
             <button onClick = {contact} className="contact-btn">CONTACT</button> 
         </div>
