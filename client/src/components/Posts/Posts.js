bbe4258c95988b31e02be294bd77fdf1ba93e356
sherriefaylin <linsherrie@gmail.com>
2019-02-07 03:48:40 -0800

    new posts

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -2,10 +2,16 @@ import React from 'react';
 import "../Posts/posts.css";
 // import ReactDOM from 'react-dom';
 import oscar from "../Posts/oscar-prof_1.png";
-
-
+import dummy from "../Posts/dummyphoto.png"
+import bigoscar from "../Posts/bigoscar.png"
+import mediumoscar from "../Posts/anotheroscar.png"
 
 class Post extends React.Component {
+    state = {
+        numChildren: 0
+    }
+
+    
     render() {
     // uh import all posts?
 
@@ -27,6 +33,28 @@ class Post extends React.Component {
             <p className="post-descr">{description}</p>
             <button onClick = {contact} className="contact-btn">CONTACT</button> 
         </div>
+
+        <div className="post-box">
+            <h2 className="post-head">02/12 GARBAGE HELP</h2>
+            <img className="post-pic" width="75px" src={bigoscar} alt="oscar lol"></img>
+            <p className="post-descr">Looking for someone to help bring my garbage bins into the backyard after 5pm.</p>
+            <button onClick = {contact} className="contact-btn">CONTACT</button> 
+        </div>
+
+        <div className="post-box">
+            <h2 className="post-head">TRASH BINS FOR 3 WEEKS</h2>
+            <img className="post-pic" width="75px" src={dummy} alt="oscar lol"></img>
+            <p className="post-descr">Will be on a business trip for 3 weeks in March. Please contact if you can help with some household duties!</p>
+            <button onClick = {contact} className="contact-btn">CONTACT</button> 
+        </div>
+
+        <div className="post-box">
+            <h2 className="post-head">TRASH HELP TOMORROW</h2>
+            <img className="post-pic" width="75px" src={mediumoscar} alt="oscar lol"></img>
+            <p className="post-descr">Broke my ankle :'( Need help with trash bins tomorrow night.</p>
+            <button onClick = {contact} className="contact-btn">CONTACT</button> 
+        </div>
+
         </div>
     )
 
